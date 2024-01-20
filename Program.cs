7f25459b973e39b1b240cc175a13842de5837d55
staslif <149203375+staslif@users.noreply.github.com>
2024-01-20 09:01:09 +0300

    Program.cs

--- Program.cs
+++ Program.cs
@@ -4,13 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Ввод строки с избыточными пробелами
         Console.WriteLine("Введите строку с избыточными пробелами:");
         string input = Console.ReadLine();
-
         // Удаление избыточных пробелов
         string output = Regex.Replace(input, @"\s+", " ");
-
         // Вывод результата
         Console.WriteLine("Результат:");
         Console.WriteLine(output);
